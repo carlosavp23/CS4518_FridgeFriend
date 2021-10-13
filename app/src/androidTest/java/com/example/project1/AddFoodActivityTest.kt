e18b5241328fd4497e7c44bd375f562a4da60848
lewasserman <laurenewasserman@gmail.com>
2021-10-12 21:59:36 -0400

    Files for junit tests

---
+++ app/src/androidTest/java/com/example/project1/AddFoodActivityTest.kt
@@ -0,0 +1,16 @@
+package com.example.project1
+
+import junit.framework.TestCase
+
+class AddFoodActivityTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+
+    fun testOnCreate() {}
+
+    fun testOnStart() {}
+}
---
+++ app/src/androidTest/java/com/example/project1/SaveActivityTest.kt
@@ -0,0 +1,14 @@
+package com.example.project1
+
+import junit.framework.TestCase
+
+class SaveActivityTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    public override fun tearDown() {}
+
+    fun testOnCreate() {}
+}
